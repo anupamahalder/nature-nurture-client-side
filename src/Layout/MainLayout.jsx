f16d943a38f183a9b19ee727b6dbf1ae51c496ac
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-13 00:27:29 +0530

    Added layout for the navbar

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,6 +1,6 @@
-// import PropTypes from 'prop-types';
-import { Link, Outlet } from 'react-router-dom';
-const MainLayout = () => {
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+const MainLayout = ({children}) => {
     return (
         <div>
             <div className="drawer text-[#466D21] bg-fixed">
@@ -13,7 +13,7 @@ const MainLayout = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                     </label>
                 </div> 
-                <div className="flex-1 px-2 mx-2">
+                <div className="flex-1 px-2 mx-2 absolute right-1 md:relative">
                     <img className='w-[50px]'
                      src="./../../public/natureLight.png" alt="" />
                     <h1 className='font-extrabold ml-2 text-3xl font-dancingScript'>Nature Nurture</h1>
@@ -21,20 +21,26 @@ const MainLayout = () => {
                 <div className="flex-none hidden lg:block">
                     <ul className="menu menu-horizontal">
                     {/* Navbar menu content here */}
+                    <li><Link to='/'>Home</Link></li>
                     <li><Link to='/about'>About</Link></li>
-                    <li><a>Navbar Item 2</a></li>
+                    <li><Link to='/contact'>Contact Us</Link></li>
+                    <li><Link to='/login'>Login</Link></li>
+                    <li><Link to='/register'>Register</Link></li>
                     </ul>
                 </div>
                 </div>
                 {/* Page content here */}
-                <Outlet></Outlet>
+                {children}
             </div> 
             <div className="drawer-side">
                 <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
-                <ul className="menu p-4 w-80 min-h-full bg-base-200">
+                <ul className="menu p-4 w-60 min-h-full bg-base-200">
                 {/* Sidebar content here */}
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/contact'>Contact Us</Link></li>
+                <li><Link to='/login'>Login</Link></li>
+                <li><Link to='/register'>Register</Link></li>
                 </ul>
             </div>
             </div>
@@ -42,7 +48,7 @@ const MainLayout = () => {
     );
 };
 // Adding propTypes 
-// MainLayout.propTypes={
-//     children: PropTypes.node.isRequired,
-// }
+MainLayout.propTypes={
+    children: PropTypes.node,
+}
 export default MainLayout;
--- src/Router/MyRoute.jsx
+++ src/Router/MyRoute.jsx
@@ -1,11 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Outlet, createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const MyRoute = createBrowserRouter([
     {
         path: '/',
-        element: <MainLayout></MainLayout>,
+        element: <MainLayout><Outlet></Outlet></MainLayout>,
         children: [
             {
                 path: '/',
@@ -14,8 +16,20 @@ const MyRoute = createBrowserRouter([
             {
                 path: 'about',
                 element: <h1>About</h1>
+            },
+            {
+                path: 'contact',
+                element: <h1>Contact us</h1>
             }
         ]
+    },
+    {
+        path: '/login',
+        element: <Login></Login>
+    },
+    {
+        path: '/register',
+        element: <Register></Register>
     }
 ])
 
