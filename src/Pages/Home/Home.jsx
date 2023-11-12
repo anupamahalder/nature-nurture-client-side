13bcb1dd72f4c5fd8da51968f7ca3491664ed24f
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-12 14:05:20 +0530

    Added drawer navbar to mainlayout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,11 @@
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            {/* banner section  */}
+            <div className="h-600px">
+                <img className="object-cover object-center"
+                 src="https://plus.unsplash.com/premium_photo-1682088329107-20def6a7c2e6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </div>
         </div>
     );
 };
