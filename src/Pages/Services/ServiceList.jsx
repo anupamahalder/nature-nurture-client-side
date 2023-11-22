37da40d5d36275c5ea188b8fe516be3156821d08
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-22 13:28:28 +0530

    Resized the service card

--- src/Pages/Services/ServiceList.jsx
+++ src/Pages/Services/ServiceList.jsx
@@ -15,7 +15,7 @@ const ServiceList = () => {
     },[]);
     return (
         
-        <div className="px-2 md:px-10 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 my-10">
+        <div className="px-2 md:px-10 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
             {
                 services?.map(service =><Services key={service._id} service={service}></Services>)
             }
--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -6,17 +6,17 @@ const Services = ({service}) => {
     const {_id,name,description,price,duration,availability,rating,reviews,guarantee,image} = service;
     return (
         <div className='mb-16 mx-auto'>
-            <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-80 md:w-96 rounded-xl bg-clip-border">
+            <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-80 md:w-[360px] rounded-xl bg-clip-border">
             <div className="relative h-[200px] mx-4 -mt-6 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
                 <img
                 src={image}
                 />
             </div>
             <div className="p-6">
-                <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+                <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900 h-[50px]">
                 {name}
                 </h5>
-                <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit h-[70px] overflow-hidden">
+                <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit h-[130px] overflow-hidden">
                 {description}
                 </p>
                 <p className='font-bold p-2'>Price: ₹ {price}</p>
